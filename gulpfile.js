d427e40ebbc3aa6be0d408367550fcd4ba3c10b3
Sagar <sagarailani3@gmail.com>
2017-12-11 17:11:34 +0530

    Styling for the website part(1)

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+// Everything in gulp revolves around tasks.
+
+
+// task(a,b)
+// a : name of the task
+// b : what should happen when this task is run
